1607a1eccdb9684623676f2d518491f2b17a6868
David <jesusd9perez@gmail.com>
2025-05-10 23:34:31 -0500

    Initial Commit: Proyecto Base

---
+++ Taller/src/main/java/com/Innovacion/Taller/TallerApplication.java
@@ -0,0 +1,14 @@
+package com.Innovacion.Taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerApplication.class, args);
+		System.out.println("FUNCIONA");
+	}
+
+}
