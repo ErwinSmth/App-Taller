3af8cce21c2bde227a6af9e2e257d00d3aae0b11
yerico <u21230122@utp.edu.pe>
2025-05-13 18:56:42 -0500

    4 Clases Cate,Estu,Orga,Profe y Taller con sus PK

---
+++ Taller/src/main/java/com/Innovacion/Taller/persistence/entity/Estudiante.java
@@ -0,0 +1,25 @@
+package com.Innovacion.Taller.persistence.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Table(name="estudiante")
+@Data
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "estudiante_id")
+    private Long estudianteId;
+
+    //relacion de la llave foranea
+    @OneToOne
+    @JoinColumn(name="user_id",nullable = false, unique = true )
+    private Usuario usuario;
+
+    @Column (name = "intereses")
+    private String intereses;
+
+
+}
