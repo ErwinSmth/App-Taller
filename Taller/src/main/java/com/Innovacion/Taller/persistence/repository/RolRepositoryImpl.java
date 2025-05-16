01c4614a48a481b45a04005898f9c8a1f64e355b
yerico <u21230122@utp.edu.pe>
2025-05-16 15:20:46 -0500

    repositorio de rol y usuario

--- Taller/src/main/java/com/Innovacion/Taller/persistence/repository/RolRepositoryImpl.java
+++ Taller/src/main/java/com/Innovacion/Taller/persistence/repository/RolRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.Innovacion.Taller.persistence.repository;
 
+import com.Innovacion.Taller.domain.dto.RolesDto;
 import com.Innovacion.Taller.domain.repositoryInterfaces.IRolRepository;
 import com.Innovacion.Taller.persistence.crud.RolCrudRepository;
 import com.Innovacion.Taller.persistence.entity.Rol;
+import com.Innovacion.Taller.persistence.mapper.RolMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import java.util.stream.Collectors;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,14 +17,27 @@ public class RolRepositoryImpl implements IRolRepository {
 
     @Autowired
     private RolCrudRepository rolCrud;
+    @Autowired
+    private RolMapper mapper;
 
     @Override
-    public Optional<Rol> findById(Long id) {
-        return rolCrud.findById(id);
+    public RolesDto save(RolesDto rolesDto) {
+        Rol rol =mapper.toRol(rolesDto);
+        return mapper.toRolesDto(rolCrud.save(rol));
+
     }
 
     @Override
-    public List<Rol> findAll() {
-        return rolCrud.findAll();
+    public Optional<RolesDto> findById(Long id) {
+        return rolCrud.findById(id).map(Rol->mapper.toRolesDto(Rol));
+    }
+
+    //La clase List NO tiene un mettodo ".map(...) como Optional o Stream."
+    @Override
+    public List<RolesDto> findAll() {
+        return rolCrud.findAll().
+                stream().map(Rol->mapper.toRolesDto(Rol))
+                .collect(Collectors.toList());
     }
 }
+//.collect(Collectors.toList()) convierte el stream transformado de nuevo en una List<RolesDto>.
