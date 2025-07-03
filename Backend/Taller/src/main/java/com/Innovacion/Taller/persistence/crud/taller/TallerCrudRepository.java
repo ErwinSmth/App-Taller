ee5b7ad61cf41daa31f4af0848b3d52822334caa
yerico <u21230122@utp.edu.pe>
2025-07-03 01:41:52 -0500

    Ultimos cambios

--- Backend/Taller/src/main/java/com/Innovacion/Taller/persistence/crud/taller/TallerCrudRepository.java
+++ Backend/Taller/src/main/java/com/Innovacion/Taller/persistence/crud/taller/TallerCrudRepository.java
@@ -2,6 +2,8 @@ package com.Innovacion.Taller.persistence.crud.taller;
 
 import com.Innovacion.Taller.persistence.entity.taller.Taller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,5 +18,9 @@ public interface TallerCrudRepository extends JpaRepository<Taller, Long> {
 
     //Buscar taller por titulo
     List<Taller> findByTituloContainingIgnoreCase (String titulo);
+
+    @Query("SELECT t FROM Taller t WHERE t.categoria.categoriaId = :categoriaId AND " +
+            "NOT EXISTS (SELECT i FROM Inscripcion i WHERE i.taller.tallerId = t.tallerId AND i.estado = 'COMPLETADO')")
+    List<Taller> findByCategoriaIdAndNoCompletado(@Param("categoriaId") Long categoriaId);
 
 }
